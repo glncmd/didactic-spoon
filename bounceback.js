9c36cd0259af7197c912e1553caa719a721cb985
glncmd <77870057+glncmd@users.noreply.github.com>
2021-12-15 19:47:00 +0800

    Update bounceback.js

--- bounceback.js
+++ bounceback.js
@@ -1,44 +1,40 @@
 // Keyframe Bounce Back Expression
 
-e = .7;
+e = 0.7;
 g = 5000;
 nMax = 9;
-
-n = 0
-
-if (numKeys > 0){
+n = 0;
+if (numKeys > 0) {
   n = nearestKey(time).index;
   if (key(n).time > time) n--;
 }
-
-if (n > 0){
+if (n > 0) {
   t = time - key(n).time;
-  v = -velocityAtTime(key(n).time - .001)*e;
+  v = -velocityAtTime(key(n).time - 0.001) * e;
   vl = length(v);
-  
-  if (value instanceof Array){
-    vu = (vl > 0) ? normalize(v) : [0,0,0];
+
+  if (value instanceof Array) {
+    vu = vl > 0 ? normalize(v) : [0, 0, 0];
   } else {
-    vu = (v < 0) ? -1 : 1;
+    vu = v < 0 ? -1 : 1;
   }
-  
   tCur = 0;
-  segDur = 2*vl/g;
+  segDur = (2 * vl) / g;
   tNext = segDur;
   nb = 1; // number of bounces
-  while (tNext < t && nb <= nMax){
+  while (tNext < t && nb <= nMax) {
     vl *= e;
     segDur *= e;
     tCur = tNext;
     tNext += segDur;
-    nb++
+    nb++;
   }
-  if(nb <= nMax){
+  if (nb <= nMax) {
     delta = t - tCur;
-    value +  vu*delta*(vl - g*delta/2);
-  }else{
-    value
+    value + vu * delta * (vl - (g * delta) / 2);
+  } else {
+    value;
   }
 } else {
-  value
+  value;
 }
