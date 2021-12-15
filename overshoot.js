d6b131437a84d4998d375f5f78a60db2d7f00d6e
glncmd <77870057+glncmd@users.noreply.github.com>
2021-12-15 19:28:14 +0800

    Create overshoot.js

---
+++ overshoot.js
@@ -0,0 +1,20 @@
+// Keyframe Overshoot Expression
+
+freq = 3;
+decay = 5;
+
+n = 0;
+
+if (numKeys > 0){
+  n = nearestKey(time).index;
+  if (key(n).time > time) n--;
+}
+
+if (n > 0){
+  t = time - key(n).time;
+  amp = velocityAtTime(key(n).time - .001);
+  w = freq*Math.PI*2;
+  value + amp*(Math.sin(t*w)/Math.exp(decay*t)/w);
+} else {
+  value
+}
